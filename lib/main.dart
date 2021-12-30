10a8bce2d0d7d52c5b6a8bd5320aa5f06abfb7eb
Roshan Parajuli <roshan.parajuly1@gmail.com>
2021-12-30 15:29:33 +0545

    add firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:kalpaniksaathi/pages/login_page.dart';
+import 'package:kalpaniksaathi/pages/authentication/login_page.dart';
+import 'package:kalpaniksaathi/pages/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   //forcing the app to run in portrait mode
   SystemChrome.setPreferredOrientations(<DeviceOrientation>[
@@ -18,9 +21,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Kalpanik Saathi',
-      home: LoginPage(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Kalpanik Saathi',
+        home: Wrapper());
   }
 }
