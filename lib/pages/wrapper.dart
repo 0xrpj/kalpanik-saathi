cfa6d65bfd655a8115369fdf602d6ed47bc8ab2a
Roshan Parajuli <roshan.parajuly1@gmail.com>
2022-01-01 17:40:06 +0545

    add provider logic

--- lib/pages/wrapper.dart
+++ lib/pages/wrapper.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kalpaniksaathi/models/user.dart';
 import 'package:kalpaniksaathi/pages/authentication/login_page.dart';
 import 'package:kalpaniksaathi/pages/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
@@ -13,7 +15,13 @@ class Wrapper extends StatelessWidget {
     // if (_isAuthenticated) {
     //   return const Home();
     // } else {
-    return const LoginPage();
+    final MyUser? user = Provider.of<MyUser?>(context);
+
+    if (user == null) {
+      return LoginPage();
+    } else {
+      return Home();
+    }
     // }
   }
 }
