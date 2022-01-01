3fb0c0c0162db0dc5680a248df39b49f4d0c1544
Roshan Parajuli <roshan.parajuly1@gmail.com>
2022-01-01 17:38:00 +0545

    modify using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:kalpaniksaathi/models/user.dart';
 import 'package:kalpaniksaathi/pages/authentication/login_page.dart';
 import 'package:kalpaniksaathi/pages/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kalpaniksaathi/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,9 +23,14 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Kalpanik Saathi',
-        home: Wrapper());
+    return StreamProvider<MyUser?>.value(
+      catchError: (_, __) => null,
+      initialData: null,
+      value: AuthService().user,
+      child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Kalpanik Saathi',
+          home: Wrapper()),
+    );
   }
 }
