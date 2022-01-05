0beb3baa060fbf068736b29c2d3b83a947d9cd33
Roshan Parajuli <roshan.parajuly1@gmail.com>
2022-01-05 16:03:18 +0545

    add themedata

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,9 +1,34 @@
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    // primarySwatch: Colors.blue,
+    primaryColor: Colors.black,
+    // accentColor: Colors.blue[600],
+    scaffoldBackgroundColor: Colors.white,
+    colorScheme: const ColorScheme.light(),
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(color: Colors.black),
+    ));
+
+ThemeData dark = ThemeData(
+    // brightness: Brightness.dark,
+    // primarySwatch: Colors.blue,
+    primaryColor: Colors.white,
+    scaffoldBackgroundColor: const Color.fromRGBO(43, 10, 69, 0.35),
+    colorScheme: const ColorScheme.dark(),
+    textTheme: const TextTheme(
+      bodyText1: TextStyle(color: Colors.black),
+    )
+    // scaffoldBackgroundColor: const Color.fromRGBO(2, 245, 120, 0.2)
+    // scaffoldBackgroundColor: Colors.redAccent,
+    );
 
 class CustomTheme {
-  const CustomTheme();
+  CustomTheme();
 
   static const Color loginGradientStart = Color(0xFF00BCD4);
   static const Color loginGradientEnd = Color(0xFF43AF50);
