0b3401db97d2d31ce1db39147106df5d51b5ab83
Roshan Parajuli <roshan.parajuly1@gmail.com>
2022-02-13 19:04:55 +0545

    integrate messages read write

--- lib/repository/data_repository.dart
+++ lib/repository/data_repository.dart
@@ -1,28 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:kalpaniksaathi/models/posts.dart';
-
-// import '../models/pets.dart';
+import 'package:kalpaniksaathi/models/messages.dart';
 
 class DataRepository {
-  // final CollectionReference collection =
-  //     FirebaseFirestore.instance.collection('pets');
-  final CollectionReference collection =
+  final CollectionReference posts_collection =
       FirebaseFirestore.instance.collection('posts');
+  final CollectionReference messages_collection =
+      FirebaseFirestore.instance.collection('messages');
 
   Stream<QuerySnapshot> getStream() {
-    return collection.snapshots();
+    return posts_collection.snapshots();
+  }
+
+  Future<QuerySnapshot> getMessages(String id) {
+    return messages_collection
+        .where('id', isEqualTo: id)
+        .orderBy('createdAt', descending: false)
+        .get();
   }
 
   Future<DocumentReference> addPost(Post post) {
-    return collection.add(post.toJson());
+    return posts_collection.add(post.toJson());
+  }
+
+  Future<DocumentReference> addMessage(Messages message) {
+    return messages_collection.add(message.toJson());
   }
 
   void updatePost(Post post) async {
-    await collection.doc(post.postId).update(post.toJson());
+    await posts_collection.doc(post.postId).update(post.toJson());
   }
 
-  // 5
   void deletePost(Post post) async {
-    await collection.doc(post.postId).delete();
+    await posts_collection.doc(post.postId).delete();
   }
 }
