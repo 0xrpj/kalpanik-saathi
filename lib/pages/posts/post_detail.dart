20b0185d53b5583f16ef218b78e9e210f0fb347e
Roshan Parajuli <roshan.parajuly1@gmail.com>
2022-03-12 17:05:23 +0545

    integrate comments

--- lib/pages/posts/post_detail.dart
+++ lib/pages/posts/post_detail.dart
@@ -1,8 +1,11 @@
 import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:kalpaniksaathi/models/comments.dart';
 import 'package:kalpaniksaathi/repository/data_repository.dart';
+import 'package:kalpaniksaathi/services/auth.dart';
 
 late Map<String, dynamic> post_data;
 
@@ -17,10 +20,16 @@ class PostDetail extends StatefulWidget {
 
 class _PostDetailState extends State<PostDetail> {
   var repo = DataRepository();
+  final User _currentUser = AuthService().getUser();
+  final DataRepository repository = DataRepository();
+  final TextEditingController _commentController = TextEditingController();
+
+  final List<Comment> _commentData = [];
 
   @override
   void initState() {
     super.initState();
+    getComments();
   }
 
   @override
@@ -63,10 +72,10 @@ class _PostDetailState extends State<PostDetail> {
                             // width: 30,
                           ),
                         ),
-                        SizedBox(
+                        const SizedBox(
                           width: 10,
                         ),
-                        Text('roshan.parajuli says',
+                        const Text('Somebody says',
                             style: TextStyle(
                               fontFamily: 'PoppinsLight',
                               fontWeight: FontWeight.w900,
@@ -76,10 +85,9 @@ class _PostDetailState extends State<PostDetail> {
                             ))
                       ],
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
-                    // Text(post.postContent.toString()),
                     Text(post_data['postContent'].toString(),
                         style: const TextStyle(
                           fontFamily: 'WorkSansLight',
@@ -112,92 +120,18 @@ class _PostDetailState extends State<PostDetail> {
                 child: Padding(
                     padding: EdgeInsets.all(14.0),
                     child: SizedBox(
-                      width: double.infinity,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        margin: EdgeInsets.all(8.0),
-                        // elevation: 8,
-                        shadowColor: Colors.grey.withOpacity(0),
-                        // child: Text('Hello')
-                        child: Column(
-                          children: [
-                            Text(
-                              'Comments',
-                              // style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Card(
-                                  margin: EdgeInsets.all(8.0),
-                                  // elevation: 8,
-                                  shadowColor: Colors.grey.withOpacity(0.15),
-                                  child: Text('Comment1'),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Card(
-                                  margin: EdgeInsets.all(8.0),
-                                  // elevation: 8,
-                                  shadowColor: Colors.grey.withOpacity(0.15),
-                                  child: Text('Comment2'),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            SizedBox(
-                              width: double.infinity,
-                              child: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Card(
-                                  margin: EdgeInsets.all(8.0),
-                                  // elevation: 8,
-                                  shadowColor: Colors.grey.withOpacity(0.15),
-                                  child: Text('Comment3'),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ))),
-            Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                elevation: 18,
-                shadowColor: Colors.grey.withOpacity(0.15),
-                child: Padding(
-                    padding: EdgeInsets.all(14.0),
-                    child: SizedBox(
                         width: double.infinity,
                         child: Column(
-                          children: const [
+                          children: [
                             Padding(
                               padding: EdgeInsets.all(8.0),
                               child: TextField(
-                                // controller: _postBodyController,
-                                // style: TextStyle(
-                                //   height: 5.0,
-                                // ),
-
+                                controller: _commentController,
+                                style: const TextStyle(
+                                  height: 2.0,
+                                ),
                                 maxLines: null,
-                                decoration: InputDecoration(
+                                decoration: const InputDecoration(
                                     border: InputBorder.none,
                                     labelText: 'Write something',
                                     labelStyle: TextStyle(
@@ -211,8 +145,76 @@ class _PostDetailState extends State<PostDetail> {
                                     ),
                               ),
                             ),
+                            ElevatedButton(
+                                onPressed: () {
+                                  final Comment newComment = Comment(
+                                      postId: widget.postId,
+                                      userId: _currentUser.uid.toString(),
+                                      commentContent: _commentController.text,
+                                      createdAt: DateTime.now()
+                                          .millisecondsSinceEpoch);
+                                  repository.addComment(newComment);
+
+                                  setState(() {
+                                    _commentData.insert(0, newComment);
+                                  });
+
+                                  _commentController.clear();
+                                },
+                                child: Text('Comment'))
                           ],
-                        ))))
+                        )))),
+            Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                elevation: 18,
+                shadowColor: Colors.grey.withOpacity(0.15),
+                child: Padding(
+                    padding: EdgeInsets.all(14.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        margin: EdgeInsets.all(8.0),
+                        // elevation: 8,
+                        shadowColor: Colors.grey.withOpacity(0),
+                        // child: Text('Hello')
+                        child: const Text(
+                          'Comments',
+                        ),
+                      ),
+                    ))),
+            Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: AlwaysScrollableScrollPhysics(),
+                  itemCount: _commentData.length,
+                  itemBuilder: (context, index) {
+                    // return ;
+                    return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        margin: const EdgeInsets.all(8.0),
+                        elevation: 8,
+                        shadowColor: Colors.grey.withOpacity(0.15),
+                        child: Padding(
+                          padding: const EdgeInsets.all(14.0),
+                          child: Column(
+                              // text align left
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(_commentData[index].commentContent
+                                    as String)
+                              ]),
+                        ));
+
+                    // List view for comments
+                  }),
+            )
           ],
         )),
       )),
@@ -227,6 +229,23 @@ class _PostDetailState extends State<PostDetail> {
 
     setState(() {
       post_data = data;
+    });
+  }
+
+  Future getComments() async {
+    final QuerySnapshot<Map<String, dynamic>> data = await FirebaseFirestore
+        .instance
+        .collection('comments')
+        .where('postId', isEqualTo: widget.postId)
+        .orderBy('createdAt', descending: true)
+        .get();
+
+    data.docs.forEach((QueryDocumentSnapshot<Map<String, dynamic>> doc) {
+      final Comment commentData = Comment.fromSnapshot(doc);
+      setState(() {
+        _commentData.add(commentData);
+        print(commentData);
+      });
     });
   }
 }
